53e1f6ec5bfe6023245c8cfcec4a4221f9645024
xleonhunter <xleonhunter@gmail.com>
2022-04-15 10:44:56 -0400

    updated pdf

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-portNumber=4000
+portNumber=4002
 echo "The application should serve on [localhost:$portNumber](http://localhost:$portNumber/) by default."
 echo "Building project..."
 
@@ -6,6 +6,6 @@ kill -kill `lsof -t -i tcp:$portNumber`
 bundle install
 bundle update --bundler
 bundle update faraday
-bundle exec jekyll serve --watch
+bundle exec jekyll serve --watch --port $portNumber
 
 echo "The application should be served on [localhost:$portNumber](http://localhost:$portNumber/)"
